c3ece3799b139af0a6ab94d79a74530483df4591
Tsvetan <tsvetanml@gmail.com>
2025-02-11 12:38:19 +0100

    add modules to app module: Auth, Prisma, and Profile

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, PrismaModule, ProfileModule],
   controllers: [AppController],
   providers: [AppService],
 })
