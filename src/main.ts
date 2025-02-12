ed16ca581a4a7a2622ee8b9e60bd08653d50605f
Tsvetan <tsvetanml@gmail.com>
2025-02-12 22:49:34 +0100

    enhance server bootstrap: enable raw request body and change listening port to 4000

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true, // Enable raw request body
+  });
+
+  await app.listen(4000);
+  console.log(`🚀 Server running on http://localhost:4000`);
 }
 bootstrap();
