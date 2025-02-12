fa09983d3742ba811b92a585dc5865b9a2f43691
Tsvetan <tsvetanml@gmail.com>
2025-02-12 22:48:45 +0100

    enhance authentication controller: add refresh and logout endpoints, support user roles in registration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,34 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('register')
-  async register(@Body() body: { email: string; password: string }) {
-    return this.authService.register(body.email, body.password);
+  async register(
+    @Body() body: { email: string; password: string; role?: 'USER' | 'ADMIN' },
+  ) {
+    return this.authService.register(body.email, body.password, body.role);
   }
 
   @Post('login')
   async login(@Body() body: { email: string; password: string }) {
     return this.authService.login(body.email, body.password);
+  }
+
+  @Post('refresh')
+  async refresh(@Body() body: { refresh_token: string }) {
+    return this.authService.refreshToken(body.refresh_token);
+  }
+
+  @Post('logout')
+  @UseGuards(JwtAuthGuard)
+  async logout(@Request() req) {
+    return this.authService.logout(
+      req.user.id,
+      req.headers.authorization.split(' ')[1],
+    );
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { LoggerModule } from 'src/logger/logger.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -9,6 +10,7 @@ import { AuthService } from './auth.service';
   imports: [
     PrismaModule,
     PassportModule,
+    LoggerModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'supersecretkey',
       signOptions: { expiresIn: '1h' },
