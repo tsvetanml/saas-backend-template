32e28846bd4aea5b5bd24e3a2ad6cb34d702ccfe
Tsvetan <tsvetanml@gmail.com>
2025-02-13 15:31:53 +0100

    add global configuration and modules: integrate ConfigModule, LoggerModule, StripeModule, and SubscriptionModule into the main application module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { LoggerModule } from './logger/logger.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProfileModule } from './profile/profile.module';
+import { StripeModule } from './stripe/stripe.module';
+import { SubscriptionModule } from './subscription/subscription.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, ProfileModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    PrismaModule,
+    ProfileModule,
+    LoggerModule,
+    StripeModule,
+    SubscriptionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
